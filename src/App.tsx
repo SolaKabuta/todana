01dd66518dcf2bfc5ac40f2ec4b714fa5ed4b5dd
Sola Kabuta <solakabuta@gmail.com>
2025-12-12 13:57:01 +0100

    refactor: class mates components removed

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
-import Footer from "./components/layout/Footer";
+// import Footer from "./components/layout/Footer";
 // import './App.css'
 import Home from "./pages/Home";
-import Gallery from "./pages/Gallery";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
-import ProductPage from "./pages/ProductPage";
+// import Gallery from "./pages/Gallery";
+// import About from "./pages/About";
+// import Contact from "./pages/Contact";
+// import ProductPage from "./pages/ProductPage";
 // import 404 from "./pages/404";
 
 function App() {
@@ -17,12 +17,12 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/gallery" element={<Gallery />} />
-        <Route path="/produit/:id" element={<ProductPage />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
+        {/*<Route path="/gallery" element={<Gallery />} />*/}
+        {/*<Route path="/produit/:id" element={<ProductPage />} />*/}
+        {/*<Route path="/about" element={<About />} />*/}
+        {/*<Route path="/contact" element={<Contact />} />*/}
       </Routes>
-      <Footer />
+      {/*<Footer />*/}
     </>
   );
 }
