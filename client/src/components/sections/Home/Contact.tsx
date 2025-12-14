dbeb6aa13e469319067d842172e21dadd189c3ae
Sola Kabuta <solakabuta@gmail.com>
2025-12-15 00:19:19 +0100

    style: contact component layout update

--- client/src/components/sections/Home/Contact.tsx
+++ client/src/components/sections/Home/Contact.tsx
@@ -2,6 +2,7 @@ import { useRef } from "react";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { Button } from "@/components/ui/button";
 
 gsap.registerPlugin(ScrollTrigger);
 // interface Testimonials {
@@ -18,9 +19,10 @@ export default function Contact() {
   return (
     <main
       ref={containerRef}
-      className="h-screen w-screen bg-primary text-white pt-5"
+      className="min-h-screen w-screen bg-primary text-white p-10"
     >
-      <div className="flex gap-3 items-center">
+      {/* -- Title Section -- */}
+      <div className="flex justify-center gap-3 items-center">
         <img
           className="rotation"
           src="/assets/icons/wheel_white.svg"
@@ -33,6 +35,27 @@ export default function Contact() {
           <span className="text-xs align-top">(08)</span>
         </h1>
       </div>
+      {/* -- Contact Form Section -- */}
+      <section className="grid md:grid-cols-2 gap-10">
+        {/* -- Banner Image -- */}
+       <div className="h-60 md:h-96">
+         <img className="object-cover h-full w-full rounded-2xl" src="/assets/images/contact/sofa.png" alt="" />
+       </div> 
+       {/* -- Form -- */}
+       <form action="" className="bg-accent text-primary rounded-2xl flex flex-col gap-1 p-10 [&_input]:bg-white [&_input]:rounded-lg">
+         <p className="text-3xl">Let's have a chat</p>
+         {/* -- Divider -- */}
+         <div className="bg-primary/10 h-0.5 w-full "></div>
+         <input className="p-5" type="text" placeholder="What's your question"/>
+         <input className="p-5" type="email" placeholder="Email *"/>
+         <input className="px-5 py-40 mb-5" type="textarea" placeholder="Describe your project, we'll bring it to life"/>
+         <Button >Submit </Button>
+       </form>
+      </section>
+      {/* -- MAP Section API ? -- */}
+      <section>
+        <div className="bg-accent"></div>
+      </section>
     </main>
   );
 }
